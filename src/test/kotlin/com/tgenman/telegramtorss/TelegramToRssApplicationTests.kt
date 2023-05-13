b662eaa7dbe416003fd9c74d837c2e2eabfee416
Dmitrii Bondarev <dmitrii.bondarev@convexitydmcc.com>
2023-05-13 15:35:25 +0800

    Init commit 2

---
+++ src/test/kotlin/com/tgenman/telegramtorss/TelegramToRssApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tgenman.telegramtorss
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TelegramToRssApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
